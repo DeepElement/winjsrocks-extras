9f20ac538ba1e3227dcf9fd01b9b3e222a662b1b
Todd Morrison <todd@deepelement.com>
2015-11-04 06:42:50 -0800

    BugFix is platform plugin registration

--- src/feature/platform-feature.js
+++ src/feature/platform-feature.js
@@ -1,8 +1,9 @@
 import WinJSRocks from "winjsrocks";
+import Platform from "platform";
 
 export default class extends WinJSRocks.Plugin.Base {
   constructor(application) {
-    super(application);
+    super(application, "platformFeatures", "runtime");
   }
 
   loadComponent(options, callback) {
@@ -11,16 +12,16 @@ export default class extends WinJSRocks.Plugin.Base {
         return callback(err);
 
       var body = document.body;
-      body.setAttribute("data-platform-description", platform.description);
-      body.setAttribute("data-platform-layout", platform.layout);
-      body.setAttribute("data-platform-manufacturer", platform.manufacturer);
-      body.setAttribute("data-platform-name", platform.name);
-      body.setAttribute("data-platform-prerelease", platform.prerelease);
-      body.setAttribute("data-platform-product", platform.product);
-      body.setAttribute("data-platform-ua", platform.ua);
-      body.setAttribute("data-os-architecture", platform.os.architecture);
-      body.setAttribute("data-os-family", platform.os.family);
-      body.setAttribute("data-os-version", platform.os.version);
+      body.setAttribute("data-platform-description", Platform.description);
+      body.setAttribute("data-platform-layout", Platform.layout);
+      body.setAttribute("data-platform-manufacturer", Platform.manufacturer);
+      body.setAttribute("data-platform-name", Platform.name);
+      body.setAttribute("data-platform-prerelease", Platform.prerelease);
+      body.setAttribute("data-platform-product", Platform.product);
+      body.setAttribute("data-platform-ua", Platform.ua);
+      body.setAttribute("data-os-architecture", Platform.os.architecture);
+      body.setAttribute("data-os-family", Platform.os.family);
+      body.setAttribute("data-os-version", Platform.os.version);
 
       return callback();
     })
