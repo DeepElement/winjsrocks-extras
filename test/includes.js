10c1fe545a00160f02e0f9dc552685e1412286b4
Todd Morrison <todd@deepelement.com>
2015-11-06 16:46:17 -0800

    added baseline unit tests and stuf

---
+++ test/includes.js
@@ -0,0 +1,27 @@
+var common = require('./common'),
+  assert = require('assert'),
+  should = require('should');
+
+describe('Integration', function() {
+  describe('Includes', function() {
+    it('verify types', function(done) {
+      // arrangec
+      var WinJSRocks = require('winjsrocks');
+      var WinJSRocksExtras = require('../src/main');
+
+
+      // assert
+      for (var pluginKey in WinJSRocksExtras.Plugin) {
+        var classDef = WinJSRocksExtras.Plugin[pluginKey];
+        classDef.prototype.should.be.an.instanceof(WinJSRocks.Plugin.Base);
+      }
+
+      for (var providerKey in WinJSRocksExtras.Provider) {
+        var classDef = WinJSRocksExtras.Provider[providerKey];
+        classDef.prototype.should.be.an.instanceof(WinJSRocks.Provider.Base);
+      }
+
+      return done();
+    });
+  });
+});
